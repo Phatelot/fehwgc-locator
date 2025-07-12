b7628dc6df1cd6fef48279a1f4299e8299971cf3
Phatelot <phatelot@outlook.fr>
2025-07-12 17:26:07 +0200

    fix assets path for image

--- src/lib/Locator.svelte
+++ src/lib/Locator.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+    import { getPublicImageLink } from "./asset_utils";
     import ZoomPanImage from "./ZoomPanImage.svelte";
 </script>
 
-<ZoomPanImage scale={1} src={"/image.webp"} x={0} y={0} />
+<ZoomPanImage scale={1} src={getPublicImageLink("spread.webp")} x={0} y={0} />
